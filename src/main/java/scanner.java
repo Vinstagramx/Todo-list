e7caa724f47835ce3a37d6e0a39808e9638c1ac8
Vincent Wong <vincent.wong@alfasystems.com>
2022-10-10 14:18:34 +0100

    First Commit

---
+++ src/main/java/scanner.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class scanner {
+
+    public static void runToDo(String[] args) {
+        Scanner commandObj = new Scanner(System.in);
+        System.out.println("Welcome to the to-do list command line tool.");
+
+//      String formatter for left-justified text in terminal
+        String format = "%-20s%s%n";
+        Boolean run = true;
+        while(run) {
+            //      Parsing user input and splitting command arguments by spaces
+            String userInput = commandObj.nextLine().toLowerCase();
+            String[] commandArgs = userInput.split("\\s+");
+            switch (commandArgs[0]) {
+                case "todo":
+                    switch (commandArgs[1]) {
+                        case "new":
+                            System.out.println("Creating new to-do item...");
+                    }
+                    break;
+                case "help":
+                    System.out.println("Commands available:");
+                    System.out.println();
+                    System.out.printf(format, "ls:", "Display full to-do list.");
+                    System.out.println();
+                    System.out.printf(format, "todo:", "Access and edit to-do list items.");
+                    System.out.println("^ sub-commands:");
+                    System.out.printf(format, "new:", "Create new to-do list item.");
+                    System.out.printf(format, "delete:", "Delete specified to-do list item.");
+                    System.out.printf(format, "clear:", "Clear all to-do list items.");
+                    System.out.println();
+                    System.out.printf(format, "exit:", "Exit to-do list program.");
+                    break;
+                case "exit":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Command %s invalid. Please enter a different command!".format(args[0]));
+            }
+        }
+
+//        Use SQLite database/postgres
+//        Java lists
+        if(args.length > 2){
+            throw new RuntimeException("More than two arguments. Please try again!");
+        }
+    }
+}
